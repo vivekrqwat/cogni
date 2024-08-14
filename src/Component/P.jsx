6c875e864d4d9dab1f19555cb51071a8d78bd2e1
vivek <136249117+vivekrqwat@users.noreply.github.com>
2024-08-14 14:27:37 +0530

    Update P.jsx

--- src/Component/P.jsx
+++ src/Component/P.jsx
@@ -2,16 +2,23 @@ import { Box, Paper, Switch, Typography } from '@mui/material'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import ModeIcon from '@mui/icons-material/Mode';
 import IosShareIcon from '@mui/icons-material/IosShare';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ContentPasteOffSharp } from '@mui/icons-material';
 
 
 
-function P({key,color,c1,c,c2,d}) {
+function P({key1,color,c1,c,c2,d,ch}) {
     // console.log(c,c1)
-    console.log("c1:",c1)
+    console.log("c1:",key1)
+    const[check,setcheck]=useState(false)
+  
+    const handelcheck=()=>{
+      setcheck(!check);
+    
+    }
+ 
   return (
   <Paper sx={{
     height:{sm:"109px",xs:"40%"},display:"flex",
@@ -30,9 +37,10 @@ function P({key,color,c1,c,c2,d}) {
     sx={{background:"#f8f9fa"}}>
 
         <Box width={{sm:"75px",xs:"30%"}}  borderRadius={"0px 0px 5px ;"}  height={"30px"} display={"flex"}
-        color={color}
-        bgcolor={c2}
-        justifyContent={"center"} alignItems={"center"} fontWeight={"600"} letterSpacing={"-0.3px"}>{c}</Box>
+        color={"black"}
+        bgcolor={check?"#b8c0ff":"#e9ecef"}
+        justifyContent={"center"} alignItems={"center"} fontWeight={"600"} letterSpacing={"-0.3px"}>
+          {check?"open":"close"}</Box>
         <Box width={"80%"} display={"flex"} flexDirection={"column"} justifyContent={"center"} >
             <Typography variant='h6' fontFamily={"Work Sans"} fontWeight={"500"} fontSize={"24px"} letterSpacing={"-0.2px"}
            >Challenge {c1}</Typography>
@@ -46,23 +54,23 @@ function P({key,color,c1,c,c2,d}) {
     // border={"2px solid red"}
     >
         <Typography sx={{width:{sm:"73%",xs:"60%"}}}>
-            {/* Background Details:what needs to be done and why,what needs to be disruted .lorem dolar sit amet */}
+            {ch}
         </Typography>
     </Box>
     <Box width={{sm:"30%",xs:"25%"}} display={"flex"} justifyContent={"space-evenly"} alignItems={"center"}
     // border={"2px solid red"}
     >
         <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
-        {c==="open"?<Switch defaultChecked></Switch>:<Switch ></Switch>}
+        <Switch checked={check} onClick={handelcheck}></Switch>
        <Typography
-       sx={{color:color}}>
-        {c}</Typography> 
+       sx={{color:"black"}}>
+        {check?"open":"close"}</Typography> 
         </Box>
   
     <Paper
     sx={{
   display:"flex" ,justifyContent:"center", alignItems:"center",height:"40px" ,width:"40px", borderRadius:"5px"
-    }} onClick={()=>{d(key)}}>
+    }} onClick={()=>{d(c1)}}>
     <svg  style={{height:"16px",width:"14,4"}}
     xmlns="http://www.w3.org/2000/svg" id="delete" x="0" y="0" version="1.1" viewBox="0 0 29 29" xml:space="preserve"><path d="M19.795 27H9.205a2.99 2.99 0 0 1-2.985-2.702L4.505 7.099A.998.998 0 0 1 5.5 6h18a1 1 0 0 1 .995 1.099L22.78 24.297A2.991 2.991 0 0 1 19.795 27zM6.604 8 8.21 24.099a.998.998 0 0 0 .995.901h10.59a.998.998 0 0 0 .995-.901L22.396 8H6.604z"></path><path d="M26 8H3a1 1 0 1 1 0-2h23a1 1 0 1 1 0 2zM14.5 23a1 1 0 0 1-1-1V11a1 1 0 1 1 2 0v11a1 1 0 0 1-1 1zM10.999 23a1 1 0 0 1-.995-.91l-1-11a1 1 0 0 1 .905-1.086 1.003 1.003 0 0 1 1.087.906l1 11a1 1 0 0 1-.997 1.09zM18.001 23a1 1 0 0 1-.997-1.09l1-11c.051-.55.531-.946 1.087-.906a1 1 0 0 1 .905 1.086l-1 11a1 1 0 0 1-.995.91z"></path><path d="M19 8h-9a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h9a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1zm-8-2h7V4h-7v2z"></path></svg>
     </Paper>
